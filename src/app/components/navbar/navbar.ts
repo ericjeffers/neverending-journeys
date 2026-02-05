8ff89fea78479b8072534d4f959ba8499c81a77f
Eric Jeffers <ericjjeffers@gmail.com>
2026-02-05 10:54:10 -0500

    Second attempt at building the site.

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+  isOpen = signal(false);
+
+  toggle() {
+    this.isOpen.set(!this.isOpen());
+  }
+}
