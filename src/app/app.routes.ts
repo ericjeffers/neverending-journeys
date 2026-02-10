5b2830ecb377eb4972b54e1949d826e191c907d1
Eric Jeffers <ericjjeffers@gmail.com>
2026-02-09 23:22:22 -0500

    Added the get a quote menu option with a google form embedded.  Added the background image and set it globally throughout the site.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
+import { Quote } from './pages/quote/quote';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
+  { path: 'quote', component: Quote },
 ];
